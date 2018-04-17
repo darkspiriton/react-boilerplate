f905453af4ddf1dc1dd077b7c2824181200bd472
Cesar Moreno <cesar200526@gmail.com>
2018-04-17 00:39:45 -0500

    update webpack config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,8 @@ class App extends Component {
   render () {
     return (
       <div>
-        <h1 className="title">My React App! {this.props.name}</h1>
+        <h1 className="title">My React App {this.props.name}</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png" alt="Logo React"/>
       </div>
     )
   }
